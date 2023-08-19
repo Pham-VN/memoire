e034ab17a90205f549d89f425e0e7a8183728aae
Pham-VN <thatnhitam@gmail.com>
2023-08-19 10:04:51 +0200

    Test_stanza_ajouter encore une dictionnaire des analyses des phrases (Sujet, Verbe, Objet)

--- test_stanza.py
+++ test_stanza.py
@@ -11,21 +11,27 @@ corpus_COD = [
     "J'aime la musique.",
     "Je le mange.",
     "Je la connais.",
+    "Il me verra.",
     "Il les prend dans ses bras.",
+    "Je me regarde dans le reflet de l’eau.",
 ]
 
 corpus_COI = [
     "Pierre lui parle.",
     "J'en parle.",
+    "Je te ressemble.",
+    "J'y pense.",
+    "J'en moque.",
+    "Je te demande de venir.",
+    "Il s’en souvient.",
+    "Je t’ai dit que c’était urgent."
 ]
 
+function_grammaticale = ["COD","Compléments du nom","Complément de lieu","COI", "Complément circonstanciel de cause", "Complément de l'adjectif", "Complément d'agent"]
 
-function_grammaticale = ["COD","COI", "Complément circonstanciel", "Attribut du Sujet", "Complément d'agent"]
-# # Mélanger des éléments dans la functionne grammaticale pour avoir des résultat différent
-reponseRandom = random.shuffle(function_grammaticale)
 # #Créer une liste de Quiz qui contient consigne, phrase et réponse
 quiz = []
-consigne = "Donner la fonction de l'élément figurant en gras, dans la phrase:"
+consigne = "Donner la fonction de l'élément figurant en rouge, dans la phrase:"
 phrase = ''
 reponse = ''
 
@@ -37,7 +43,13 @@ fonction_COI = "iobj"
 
 # # Parcourir chaque phrase dans le corpus et annoterdeprel
 def createSentenceWithClue(corpus,fonction):
-    list_sentence_exercice = []
+    
+    # list_sentence_exercice = []
+    
+    # analyse = []
+    
+    list_sentence_and_analyses = []
+
     for sentence in corpus:
         # dict_consigne_phrase_reponse = {}
         doc = nlp(sentence)
@@ -46,8 +58,13 @@ def createSentenceWithClue(corpus,fonction):
         words_in_first_sentence = doc.sentences[0].words
         #liste de phrase dans les exercices avec **word** pour les étudiants savent quel mot qu'il doit trouver la fonction
         for sentence in sentences:
+            
             sentence_for_exercice = ''
+            dic_sentence_analyse = {}
+            dic_word = {}
+
             for token in words_in_first_sentence:
+                    
                 idWord = token.id
                 if(token.deprel == fonction):
                     sentence_for_exercice += " " + "**" + token.text + "**"
@@ -55,45 +72,64 @@ def createSentenceWithClue(corpus,fonction):
                     sentence_for_exercice += token.text
                 else:
                     sentence_for_exercice += " " + token.text  
-            list_sentence_exercice.append(sentence_for_exercice)
-    return list_sentence_exercice
+                                    
+                if(token.deprel == "nsubj"):
+                    sujet = token.text
+                if(token.deprel == "root"):
+                    verbe = token.text
+                if(token.deprel == fonction):
+                    fonctionToken = token.text
+                    
+            # list_sentence_exercice.append(sentence_for_exercice)
 
-sentence_exercice_COD = createSentenceWithClue(corpus_COD,fonction_COD)
-sentence_exercice_COI = createSentenceWithClue(corpus_COI,fonction_COI)
+            # dic_word.update({"sujet":sujet, "verbe":verbe, fonction:fonctionToken})
+            dic_word.update({"sujet":sujet, "verbe":verbe, fonction:fonctionToken})
+            dic_sentence_analyse.update({"phrase":sentence_for_exercice,"analyse":dic_word})
+            list_sentence_and_analyses.append(dic_sentence_analyse)
+            
+    return list_sentence_and_analyses
+
+sentence_exercice_analyse_COD = createSentenceWithClue(corpus_COD,fonction_COD)
+sentence_exercice_analyse_COI = createSentenceWithClue(corpus_COI,fonction_COI)
 
 def createReponsePossible(fonction):
+
     reponses = []
-    for reponseRandom in function_grammaticale:
+    for reponseRandom in random.sample(function_grammaticale, len(function_grammaticale)):
         dic_reponse = {}
         if(reponseRandom == fonction):
             reponse = fonction
             correct = "true"
             dic_reponse.update({"texte":reponse, "correct":correct})
             reponses.append(dic_reponse)
+            
         else:
             reponse = reponseRandom
             correct = "false"
             dic_reponse.update({"texte":reponse, "correct":correct})
             reponses.append(dic_reponse)
+            
     return reponses
+
 
 listFonctionCOD = createReponsePossible("COD")
 listFonctionCOI = createReponsePossible("COI")
-
 
 # # créer un fichier JSON
 def createDictConPhraseReponse(list_sentence_exercice,consigne,listFonction):
     for sentence_exercice in list_sentence_exercice:
         dict_consigne_phrase_reponse = {}
         dict_consigne_phrase_reponse["question"] = consigne
-        dict_consigne_phrase_reponse["phrase"] = sentence_exercice
-        dict_consigne_phrase_reponse["reponses"] = listFonction
+        dict_consigne_phrase_reponse["phrase"] = sentence_exercice["phrase"]
+        dict_consigne_phrase_reponse["reponses"] = random.sample(listFonction,len(listFonction))
+        dict_consigne_phrase_reponse["analyses"] = sentence_exercice["analyse"]
+
         quiz.append(dict_consigne_phrase_reponse)
         
     return quiz
 
-createDictConPhraseReponse(sentence_exercice_COD,consigne,listFonctionCOD)
-createDictConPhraseReponse(sentence_exercice_COI,consigne,listFonctionCOI)
+createDictConPhraseReponse(sentence_exercice_analyse_COD,consigne,listFonctionCOD)
+createDictConPhraseReponse(sentence_exercice_analyse_COI,consigne,listFonctionCOI)
 
 random.shuffle(quiz)
 
